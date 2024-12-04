5b26b4128948a4154fd54730a32ba9539b4f762d
sonu79934 <yogasritha8@gmail.com>
2024-12-04 10:38:05 +0530

    Add files via upload

---
+++ checkevenoroddusingand.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class checkevenoroddusingand{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if((a&1)==0)
+        System.out.println("Even");
+        else
+        System.out.println("Odd");
+    }
+}
+
